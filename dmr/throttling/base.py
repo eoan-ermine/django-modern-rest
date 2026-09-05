8880646eb63973cfcbf9f480300e7802d143cb98
Danil Sidoruk <danilsidoruknv@yandex.ru>
2026-09-05 11:09:27 +0300

    Don't take lock on Redis cache increment

--- dmr/throttling/base.py
+++ dmr/throttling/base.py
@@ -266,7 +266,7 @@ class SyncThrottle(_BaseThrottle[BaseThrottleSyncBackend]):
         cache_key = self.full_cache_key(endpoint, controller)
         if cache_key is None:
             return
-        with lock:
+        with self._backend.lock(lock):
             self._check(endpoint, controller, cache_key)
 
     def report_usage(
@@ -330,7 +330,7 @@ class AsyncThrottle(_BaseThrottle[BaseThrottleAsyncBackend]):
         cache_key = self.full_cache_key(endpoint, controller)
         if cache_key is None:
             return
-        async with lock:
+        async with self._backend.lock(lock):
             await self._check(endpoint, controller, cache_key)
 
     async def report_usage(
